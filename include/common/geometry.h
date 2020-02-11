32b862a8d3d2a1f159d7971b238deddda56f0c70
lrodriguez15 <leopoldo.rodriguez0303@gmail.com>
2020-02-11 22:43:32 +0000

    Additional files for folder structure

---
+++ include/common/geometry.h
@@ -0,0 +1,11 @@
+// -*- lsst-c++ -*-
+
+/*
+* This product includes software developed by
+* Leopoldo Rodriguez Salazar
+* leopoldo.rodriguez0303@gmail.com
+* https://github.com/lrodriguez15
+*
+* This code is licensed under MIT license (see LICENSE.txt for details)
+*
+*/
