afc86a18b80675b3e434825c6450bf0f99ad3c9c
Leopoldo Rodriguez <leopoldo.rodriguez0303@gmail.com>
2020-02-29 21:09:57 +0000

    additional files for ci

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,19 @@
 // -*- lsst-c++ -*-
 
 /*
-* This product includes software developed by
-* Leopoldo Rodriguez Salazar
-* leopoldo.rodriguez0303@gmail.com
-* https://github.com/lrodriguez15
-*
-* This code is licensed under MIT license (see LICENSE.txt for details)
-*
-*/
+ * This product includes software developed by
+ * Leopoldo Rodriguez Salazar
+ * leopoldo.rodriguez0303@gmail.com
+ * https://github.com/lrodriguez15
+ *
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ *
+ */
 
 #include <iostream>
 
-int main() {
+int main()
+{
   std::cout << "Hello World" << '\n';
   return 0;
 }
