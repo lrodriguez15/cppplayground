8f2e748a1717636c68886a1f0aff4ceebe564f8d
Leopoldo Rodriguez <leopoldo.rodriguez0303@gmail.com>
2020-03-01 19:48:02 +0000

    clang format changes

--- src/dummy/dummy.cpp
+++ src/dummy/dummy.cpp
@@ -2,15 +2,15 @@
 
 double add_numbers(const double f1, const double f2)
 {
-    return f1 + f2;
+  return f1 + f2;
 }
 
 double subtract_numbers(const double f1, const double f2)
 {
-    return f1 - f2;
+  return f1 - f2;
 }
 
 double multiply_numbers(const double f1, const double f2)
 {
-    return f1 * f2;
+  return f1 * f2;
 }
--- test/src/dummy/dummy_test.cpp
+++ test/src/dummy/dummy_test.cpp
@@ -1,23 +1,23 @@
 #include "gtest/gtest.h"
 #include "dummy/dummy.h"
 
-TEST(example, add) // NOLINT
+TEST(example, add)  // NOLINT
 {
-    double res;
-    res = add_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, 3.0, 1.0e-11); // NOLINT
+  double res;
+  res = add_numbers(1.0, 2.0);
+  ASSERT_NEAR(res, 3.0, 1.0e-11);  // NOLINT
 }
 
-TEST(example, subtract) // NOLINT
+TEST(example, subtract)  // NOLINT
 {
-    double res;
-    res = subtract_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, -1.0, 1.0e-11); // NOLINT
+  double res;
+  res = subtract_numbers(1.0, 2.0);
+  ASSERT_NEAR(res, -1.0, 1.0e-11);  // NOLINT
 }
 
-TEST(example, multiply) // NOLINT
+TEST(example, multiply)  // NOLINT
 {
-    double res; 
-    res = multiply_numbers(1.0, 1.0);
-    EXPECT_DOUBLE_EQ(res, 1.0); // NOLINT
+  double res;
+  res = multiply_numbers(1.0, 1.0);
+  EXPECT_DOUBLE_EQ(res, 1.0);  // NOLINT
 }
